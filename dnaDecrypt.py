32b06203ace449ab05d0efc8e4257318ef67a3fd
Tyson Shepherd <trsheph@users.noreply.github.com>
2019-03-05 10:05:46 -0500

    Update dnaDecrypt.py

--- dnaDecrypt.py
+++ dnaDecrypt.py
@@ -1,8 +1,18 @@
-#
-# C. Tyson R. Shepherd, PhD
-#
 # Function container for digital file storage in DNA
+#    Copyright (C) 2019  Tyson R. Shepherd, PhD
 #
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 #
 import sys
@@ -12,7 +22,6 @@ from struct import unpack
 from struct import pack
 from struct import calcsize
 from bitstring import BitArray
-from lzma import decompress
 from Crypto.Cipher import AES
 from io import StringIO
 #
